097ac4f2d014d3d758806d28ce942232157c41c9
romainIEPS <romain.garcia@hotmail.com>
2021-01-31 19:07:06 +0100

    Mise en place du template

---
+++ resources/views/templates/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html lang="en">
+
+<head>
+
+    @include('templates.partials._head')
+
+</head>
+
+<body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+
+    {{-- HEADER --}}
+    @include('templates.partials._header')
+
+    {{-- HERO --}}
+    @include('templates.partials._hero')
+
+    {{-- RECENT WORK --}}
+    @include('templates.partials._recentWorks')
+
+    {{-- LATEST POSTS --}}
+    @include('templates.partials._latestPosts')
+
+    {{-- FOOTER --}}
+    @include('templates.partials._footer')
+	    
+    {{-- SCRIPTS --}}
+    @include('templates.partials._scripts')
+
+    </body>
+</html>

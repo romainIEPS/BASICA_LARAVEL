151766289b195eaf54b559645adf30441a82da28
romainIEPS <romain.garcia@hotmail.com>
2021-02-03 13:14:48 +0100

    Mise en place des liens sur la homepage

--- resources/views/posts/_latest.blade.php
+++ resources/views/posts/_latest.blade.php
@@ -7,11 +7,11 @@
         <h2>Latest Blog Posts</h2>
         @foreach ($posts as $post)
             <div class="row">
-                <div class="col-xs-4"><a href="blog-post.html"><img src="{{ asset('assets/img/blog/' . $post->image)}}" alt="Post Title"></a></div>
+                <div class="col-xs-4"><a href="{{ route('blog.show', ['post' => $post->id, 'slug'=> Str::slug($post->title, '-')])}}"><img src="{{ asset('assets/img/blog/' . $post->image)}}" alt="Post Title"></a></div>
                 <div class="col-xs-8">
-                    <div class="caption"><a href="blog-post.html">{{ $post->title }}</a></div>
+                    <div class="caption"><a href="{{ route('blog.show', ['post' => $post->id, 'slug'=> Str::slug($post->title, '-')])}}">{{ $post->title }}</a></div>
                     <div class="date">{{ \Carbon\Carbon::parse($post->created_at)->format('d F Y') }}</div>
-                    <div class="intro">{{ Str::limit($post->content, 150) }}<a href="blog-post.html">Read more.</a></div>
+                    <div class="intro">{{ Str::limit($post->content, 150) }}<a href="{{ route('blog.show', ['post' => $post->id, 'slug'=> Str::slug($post->title, '-')])}}">Read more.</a></div>
                 </div>
             </div>
         @endforeach
